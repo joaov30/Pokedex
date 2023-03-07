3cc4e6029f188ccb93fbca69c4be8ac164bf19d6
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2023-03-07 19:42:26 -0300

    07/03

--- pokedex/src/pages/Home.jsx
+++ pokedex/src/pages/Home.jsx
@@ -13,20 +13,39 @@ export const Home = () => {
 
   const getPokemons = () => {
     var endpoints = [];
-    for (var i = 1; i<1009; i++){
-      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+    for (var i = 1; i < 1009; i++) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
     }
-    var response = axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res));
+    axios
+      .all(endpoints.map((endpoint) => axios.get(endpoint)))
+      .then((res) => setPokemons(res));
   };
+
+  const pokemonFilter = (name) =>{
+    var filteredPokemons = [];;
+    if(name===""){
+      getPokemons()
+    }
+      for(var i in pokemons){
+        if(pokemons[i].data.name.includes(name)){
+            filteredPokemons.push(pokemons[i]);
+        }
+      }
+    setPokemons(filteredPokemons)
+  }
 
   return (
     <div>
-      <NavBar />
+      <NavBar pokemonFilter={pokemonFilter} />
       <Container maxWidth="false">
         <Grid container spacing={2}>
           {pokemons.map((pokemon, key) => (
-            <Grid item xs={2} key={key}>
-              <PokemonCard name={pokemon.data.name} image={pokemon.data.sprites.front_default} />
+            <Grid item xs={12} md={2} key={key}>
+              <PokemonCard
+                name={pokemon.data.name}
+                image={pokemon.data.sprites.front_default}
+                types={pokemon.data.types}
+              />
             </Grid>
           ))}
         </Grid>
